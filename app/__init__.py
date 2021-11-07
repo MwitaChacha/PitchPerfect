ef3662397bd3d4081b82424586780d57ee057f7a
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-07 19:24:54 +0300

    Creating index page

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from .config import DevConfig
-
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__,instance_relative_config = True)
 
-
+bootstrap = Bootstrap(app)
 from app import views
