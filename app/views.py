328780cdfe6fcda22b2e7cf4d9bad0892d7c1d92
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-07 21:38:55 +0300

    Creating view functions for the application

--- app/views.py
+++ app/views.py
@@ -7,3 +7,38 @@ from app import app
 def index():
     
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    
+    return render_template('login.html')
+
+@app.route('/success')
+def success():
+    
+    return render_template('success.html')
+
+@app.route('/failure')
+def failure():
+    
+    return render_template('failure.html')
+
+@app.route('/dashboard')
+def dashboard():
+    
+    return render_template('dashboard.html')
+
+@app.route('/pitch')
+def pitch():
+    
+    return render_template('pitch.html')
+
+@app.route('/profile')
+def profile():
+    
+    return render_template('profile.html')
