e4ee873406c1b2d962d31561ccc8bcee167e8666
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-08 06:43:28 +0300

    Importing of necessary extensions

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from flask_wtf import FlaskForm
+from .forms import LoginForm, RegisterForm, PitchForm, CommentForm
+from .models import Users, Pitch, Comment
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from . import db
 
 
 
