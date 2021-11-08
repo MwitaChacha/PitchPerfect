f5aa204c2e2d78ef49ec6fadc6badc45b952aed2
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-08 06:46:09 +0300

    Adding login authentication and hashed password

--- app/views.py
+++ app/views.py
@@ -7,17 +7,31 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from . import db
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 @app.route('/')
 def index():
     
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+       
+        new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
     
-    return render_template('signup.html')
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/success')
+    return render_template('signup.html', form=form)
 
 @app.route('/login')
 def login():
