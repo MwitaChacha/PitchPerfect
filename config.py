d38515d0a7b02d5e9351dfc07e86d75c86b5dda9
Mwita Chacha <jonahhchacha@gmail.com>
2021-11-11 10:48:05 +0300

    Adding changes to the config module

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
    
 
 class DevConfig(Config):
